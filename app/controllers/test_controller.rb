90bfdd3225ece603f77319240b55e6519c66d9c9
Nick-Brennan <nick_p_b@yahoo.com>
2018-03-06 11:57:12 -0800

    set the right app god damn it

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,20 +3,20 @@ class TestController < ApplicationController
   def test
   	Thread.new do
 	  	p "ok 1"
-	  	# var1 = system 'heroku ps --app cli-test-1'
+	  	# var1 = system 'heroku ps --app bp-test-2'
 	  	p "ok 2"
-	  	# var1 = system 'heroku ps:resize web=standard-1x --app cli-test-1'
+	  	# var1 = system 'heroku ps:resize web=standard-1x --app bp-test-2'
 	  	Thread.new do
 	  		var2 =false
 	  		while !var2
-	  			var2 = system 'heroku ps:scale web1=1 --app cli-test-1'
+	  			var2 = system 'heroku ps:scale web1=1 --app bp-test-2'
 	  			p var2
 	  		end
 	  	end
 	  	Thread.new do
 	  		var3 = false
 	  		while !var3
-	  			var3 = system 'heroku ps:scale web2=1 --app cli-test-1'
+	  			var3 = system 'heroku ps:scale web2=1 --app bp-test-2'
 	  		end
 	  		p var3
 	  	end
@@ -36,32 +36,32 @@ class TestController < ApplicationController
 	  	Thread.new do
 	  		var1 = false
 	  		while !var1
-	  			var1 = system 'heroku ps:stop web1 --app cli-test-1'
+	  			var1 = system 'heroku ps:stop web1 --app bp-test-2'
 	  			p var1
 	  		end
 	  	end
 	  	Thread.new do
 	  		var2 = false
 	  		while !var2
-	  			var2 = system 'heroku ps:scale web1=0 --app cli-test-1'
+	  			var2 = system 'heroku ps:scale web1=0 --app bp-test-2'
 	  			p var2
 	  		end
 	  	end
 	  	Thread.new do
 	  		var3 = false
 	  		while !var3
-	  			var3 = system 'heroku ps:stop web2 --app cli-test-1'
+	  			var3 = system 'heroku ps:stop web2 --app bp-test-2'
 	  			p var3
 	  		end
 	  	end
 	  	Thread.new do
 	  		var4 = false
 	  		while !var4
-	  			var4 = system 'heroku ps:scale web2=0 --app cli-test-1'
+	  			var4 = system 'heroku ps:scale web2=0 --app bp-test-2'
 	  			p var4
 	  		end
 	  	end
-	  	# var5 = system 'heroku ps:resize web=hobby --app cli-test-1'
+	  	# var5 = system 'heroku ps:resize web=hobby --app bp-test-2'
 	  	# p var5
   	end
   	render :inline => "<h1> Check Web UI </h1>"
